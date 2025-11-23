702a93eba5f5a5da2f170a7a9156b651744516fb
Amirul Luqman <miyodeskun@gmail.com>
2025-11-23 22:30:33 +0800

    Create 7. Drawer.dart

---
+++ Coding Tuto/7. Drawer.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Basic Flutter App',
+      home: Scaffold(
+        appBar: AppBar(title: Text('Home Page'), backgroundColor: Colors.blue),
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: [
+              DrawerHeader(
+                decoration: BoxDecoration(color: Colors.blue),
+                child: Text(
+                  'Drawer Header',
+                  style: TextStyle(color: Colors.white, fontSize: 24),
+                ),
+              ),
+              ListTile(
+                leading: Icon(Icons.home),
+                title: Text('Home'),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                leading: Icon(Icons.settings),
+                title: Text('Settings'),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+            ],
+          ),
+        ),
+        body: Center(
+          child: Text(
+            'Hello, World!',
+            style: TextStyle(
+              fontSize: 50,
+              color: Colors.red,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
